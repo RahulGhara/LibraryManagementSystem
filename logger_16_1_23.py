074fd45e2b69102647560f7527c77cc9c5e8f06b
Rahul Ghara <rahul.ghara@bedev.cbnits.com>
2023-02-07 18:13:08 +0530

    logger format changed

--- logger_16_1_23.py
+++ logger_16_1_23.py
@@ -1,5 +1,5 @@
 import logging
 
-LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
+LOG_FORMAT = '%(levelname)s %(asctime)s [%(filename)s:%(lineno)d -(%(funcName)s)] - %(message)s'
 logging.basicConfig(filename= 'LibraryManagementSysytem_logger.log', level= logging.DEBUG , format= LOG_FORMAT, filemode='w')
 logger = logging.getLogger()
